51d2f82753f79116431d0d62078ae57cd61bcc0c
ubii10 <Jacob202@protonmail.com>
2022-06-24 18:42:14 +0200

    enkapsulacja

---
+++ mod_2/4Enkapsulacja/main.py
@@ -0,0 +1,14 @@
+from shop.order import generate_order
+from shop.product import Product
+
+def run_homework():
+    first_order = generate_order()
+    print(first_order)
+
+    cookie = Product("Ciastko", category_name="Przekąska", unit_price=3)
+    first_order.add_product_to_order(cookie, quantity=5)
+    print(first_order)
+
+
+if __name__ == "__main__":
+    run_homework()
